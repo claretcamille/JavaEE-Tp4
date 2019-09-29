71b7f15efae61ccd35fc22cf62970b661c82a264
camilleclaret <camille.claret@etud.univ-jfc.fr>
2019-09-29 10:19:19 +0200

    Mise en place des test

---
+++ src/main/java/bowling/MultiPlayer.java
@@ -0,0 +1,28 @@
+
+package bowling;
+
+import java.util.Stack; // importation de la classe pile en java
+
+/**
+ * Implémentation de la classe MultiPlayerGame
+ * Ici je pense a une pile pour géré 
+ * @author camilleclaret
+ */
+public class MultiPlayer implements MultiPlayerGame {
+
+    @Override
+    public String startNewGame(String[] playerName) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String lancer(int nombreDeQuillesAbattues) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int scoreFor(String playerName) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+   
+}
---
+++ src/test/java/bowling/MultiPlayerTest.java
@@ -0,0 +1,74 @@
+
+package bowling;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author camilleclaret
+ */
+public class MultiPlayerTest {
+    
+    String joueur [] = {"Nicolas","Manon"};
+    MultiPlayer jeu ;
+    
+    @Before
+    public void setUp() throws Exception {
+        jeu = new MultiPlayer();
+        jeu.startNewGame(joueur);
+     }
+    
+    @Test 
+    public  void LancerPartie() {
+       assertEquals(jeu.joueur.size(), jeu.jeu.size());
+    }
+    
+    @Test 
+    public void creationExceptionNewJeu(){
+        try{
+            jeu.startNewGame(null);
+            String vide[] = new String[0];
+            jeu.startNewGame(vide);
+         }catch (Exception ex) {
+            Logger.getLogger(MultiPlayerTest.class.getName()).log(Level.SEVERE, null, ex);
+            assertEquals(jeu.joueur.size(), jeu.jeu.size());
+        }
+    }
+    
+    @Test
+    public void changementJoueur() throws Exception{
+     
+     assertEquals(joueur[0],jeu.donneJoueur());
+     jeu.lancer(10);
+     assertEquals(joueur[1],jeu.donneJoueur());
+     jeu.lancer(10);
+     assertEquals(joueur[0],jeu.donneJoueur());
+     }
+    
+    @Test
+    public void Score() throws Exception{
+        for(int i = 0;i<24;i++){
+            jeu.lancer(10);
+        }
+        assertEquals(jeu.scoreFor(joueur[1]),300);
+        assertEquals(jeu.scoreFor(joueur[0]),300);
+    }
+    
+    @Test
+    public void ScoreJoueurNonExist() throws Exception{
+        try{
+            jeu.scoreFor("Camille");
+        } catch (Exception ex) {
+            Logger.getLogger(MultiPlayerTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+    
+   
